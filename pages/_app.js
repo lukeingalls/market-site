d53329d06f5e66b8730c3f95e41397a32b600ffa
Luke Ingalls <lukeingalls@me.com>
2021-03-14 11:48:57 -0600

    feat(_app.js): added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,20 @@ function MyApp({ Component, pageProps }) {
         <title>
           Bountiful Finance - Financial News From Independent Authors
         </title>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-H1LCNW2L75"
+        ></script>
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${process.env.GOOGLE_ANALYTICS});`,
+          }}
+        />
       </Head>
       <Provider session={pageProps.session}>
         <div className="bg-light">
